a1da6931ce3d2d9099333c1de606846a01989e01
Thomas Coker <tcokerbaylor@gmail.com>
2025-12-29 11:38:47 -0600

    added docker functionality and first airflow DAG to the py files

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -2,16 +2,20 @@ import requests
 import json
 from datetime import date
 
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path="E:\github\ELT_Tutorial\.env")
 
-load_dotenv(dotenv_path="E:\github\ELT_Tutorial\.env")
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = os.getenv("CHANNEL_HANDLE")
+from airflow.decorators import task
+from airflow.models import Variable
+
+
+API_KEY = Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 max_results = 50
 
-
+@task
 def get_playlist_id():
     try:
         url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
@@ -34,6 +38,7 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
 
+@task
 def get_video_ids(playlistId):
 
     video_ids = []
@@ -72,7 +77,7 @@ def get_video_ids(playlistId):
 
 
 
-
+@task
 def extract_video_data(video_ids):
     extracted_data = []
 
@@ -118,8 +123,9 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
 
+@task
 def save_to_json(extracted_data):
-    file_path = f"E:\github\ELT_Tutorial\data\MrBeastYT_data_{date.today()}.json"
+    file_path = f".data\MrBeastYT_data_{date.today()}.json"
 
     with open(file_path, "w", encoding="utf-8") as json_outfile:
         json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
