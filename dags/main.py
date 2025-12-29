d1951a1f1094ac347350515f2bbe7cb4f0051c60
Thomas Coker <tcokerbaylor@gmail.com>
2025-12-29 12:04:04 -0600

    fixed the docker-compose.yaml to match my configs

--- dags/main.py
+++ dags/main.py
@@ -13,7 +13,7 @@ local_tz = pendulum.timezone("America/Chicago")
 
 # Default Args
 default_args = {
-    "owner":"dataengineers",
+   "owner": "dataengineers",
     "depends_on_past": False,
     "email_on_failure": False,
     "email_on_retry": False,
@@ -23,22 +23,23 @@ default_args = {
     "max_active_runs": 1,
     "dagrun_timeout": timedelta(hours=1),
     "start_date": datetime(2025, 1, 1, tzinfo=local_tz),
-    # 'end_date' : datetime(2030, 12, 31, tzinfo=local_tz), 
+    # 'end_date': datetime(2030, 12, 31, tzinfo=local_tz),
 }
 
+# DAG 1: produce_json
 with DAG(
-    dag_id='produce_json',
+    dag_id="produce_json",
     default_args=default_args,
-    description="DAG to produce JSON file with raw data.",
-    schedule=None,
-    catchup=False
-) as dag:
-    
-    # Define Tasks
+    description="DAG to produce JSON file with raw data",
+    schedule="0 14 * * *",
+    catchup=False,
+) as dag_produce:
+
+    # Define tasks
     playlist_id = get_playlist_id()
     video_ids = get_video_ids(playlist_id)
-    extracted_data = extract_video_data(video_ids)
-    save_to_json_task = save_to_json(extracted_data)
+    extract_data = extract_video_data(video_ids)
+    save_to_json_task = save_to_json(extract_data)
 
     # Define Dependencies
-    playlist_id >> video_ids >> extracted_data >> save_to_json_task
+    playlist_id >> video_ids >> extract_data >> save_to_json_task
